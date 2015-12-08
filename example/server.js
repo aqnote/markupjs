cab7278247963afdd8a7336f629f8d78caa9c80c
lipeng <madding.lip@gmail.com>
2015-12-08 18:25:56 +0800

    init

---
+++ example/server.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+'use strict';
+
+// Modules
+var debug = require('debug')('raneto');
+console.log(debug);
+
+// Here is where we load Raneto.
+// When you are in your own project repository,
+// Raneto would be installed via NPM and loaded as:
+// var raneto = require('raneto');
+var markup = require('../app/index.js');
+
+// Then, we load our configuration file
+// This can be done inline, with a JSON file,
+// or with a Node.js module as we do below.
+var config = require('./config.default.js');
+
+// Finally, we initialize Raneto
+// with our configuration object
+var app = markup(config);
+
+// Load the HTTP Server
+var server = app.listen(app.get('port'), function () {
+  debug('Express HTTP server listening on port ' + server.address().port);
+});
