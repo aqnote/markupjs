fc4cc37173a046c00984c3c656f894985abc5895
lipeng <madding.lip@gmail.com>
2015-12-11 17:25:14 +0800

    change author info

--- app/markup-context.js
+++ app/markup-context.js
@@ -19,7 +19,7 @@ var markup_context = {
 		// Toggle debug logging
 		debug: false,
     // 用户信息
-    author_info: '<a href="mailto:webmaster@example.com">御道</a>, ' + new Date(),
+    author_info: '<a href="http://aqnote.com/author">Powered by Yudao</a> <a href="mailto:webmaster@example.com">邮箱</a>, ' + new Date(),
     // 广告
     ad: ''
 	}
